5030b12dc428eb607f9b9b2ed0b4c12c1b4a9936
Shivam Shukla <77653717+shivam910@users.noreply.github.com>
2022-03-22 18:16:29 +0530

    Update Rotate Image.cpp

--- Arrays Part 2/Rotate Image.cpp
+++ Arrays Part 2/Rotate Image.cpp
@@ -1,19 +1,40 @@
 // Link: https://leetcode.com/problems/rotate-image/
 
-class Solution {
+// Optimized solution
+class Solution { 
 public:
-    void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size()-1;
-        int v[n+1][n+1];
-        for( int i=0; i<n+1; i++ ) {
-            for(int j=0; j<n+1; j++ ) {
-                v[i][j] = matrix[n-j][i];
-            }
-        }
-        for( int i=0; i<n+1; i++ ) {
-            for(int j=0; j<n+1; j++ ) {
-                matrix[i][j] = v[i][j];
-            }
+void rotate(vector<vector<int>>& matrix) {
+    int n=matrix.size();
+    for(int i=0;i<n/2;i++)
+    {
+        for(int j=i;j<n-i-1;j++)
+        {
+            int x=matrix[i][j];
+            matrix[i][j]=matrix[n-j-1][i];
+            matrix[n-j-1][i]=matrix[n-i-1][n-j-1];
+            matrix[n-i-1][n-j-1]=matrix[j][n-i-1];
+            matrix[j][n-i-1]=x;
         }
     }
+}
 };
+
+// rotation using an extra array
+// class Solution {
+// public:
+//     void rotate(vector<vector<int>>& matrix) {
+//         int n = matrix.size()-1;
+//         int v[n+1][n+1];
+//         for( int i=0; i<n+1; i++ ) {
+//             for(int j=0; j<n+1; j++ ) {
+//                 v[i][j] = matrix[n-j][i];
+//             }
+//         }
+//         for( int i=0; i<n+1; i++ ) {
+//             for(int j=0; j<n+1; j++ ) {
+//                 matrix[i][j] = v[i][j];
+//             }
+//         }
+//     }
+// };
+
